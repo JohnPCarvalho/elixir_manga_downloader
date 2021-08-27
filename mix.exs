9c306aaea335482b7a1e9f77b5d4e7e294181eb5
JohnPCarvalho <johnnypcarvalho@gmail.com>
2021-08-27 09:34:03 -0300

    Create basic structure for the project

---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule ElixirMangaDownloadr.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :elixir_manga_downloadr,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.2"},
+      {:httpotion, "~> 2.1.0"},
+      {:floki, "~> 0.31.0"}
+    ]
+  end
+end
---
+++ test/elixir_manga_downloadr_test.exs
@@ -0,0 +1,22 @@
+defmodule ElixirMangaDownloadrTest do
+  use ExUnit.Case
+  alias ElixirMangaDownloadr.IndexPage
+  alias ElixirMangaDownloadr.ChapterPage
+  doctest ElixirMangaDownloadr
+
+  @test_manga "saint-seiya-episode-g"
+  @manga_title "saint-seiya-episodeg"
+
+  @expected_chapters ["saint-seiya-episodeg-chapter-1", "saint-seiya-episodeg-chapter-1"]
+
+  @expectec_pages ["/saint-seiya-episodeg-chapter-1#1", "saint-seiya-episodeg-chapter-1#2"]
+
+  test "greets the world" do
+    assert ElixirMangaDownloadr.hello() == :world
+  end
+
+  test "get all chapters available for the manga" do
+    {:ok, manga_title, chapter_list} =
+  end
+
+end
