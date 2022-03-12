b205aa360dc2e69fe4433d69aaa6aa7388f42dad
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-03-11 22:32:22 -0300

    Add the use of Tesla HTTP for fetching pages

--- lib/elixir_manga_downloadr/chapter_page.ex
+++ lib/elixir_manga_downloadr/chapter_page.ex
@@ -1,14 +1,13 @@
 defmodule ElixirMangaDownloadr.ChapterPage do
-  @default_path "/tmp/"
+  #@default_path "/tmp/"
 
   def get_chapter_pages(chapter_link) do
-    case HTTPotion.get("mangareader.cc/chapter/#{chapter_link}") do
-      %HTTPotion.Response{body: body, headers: _headers, status_code: 200} ->
-        fetch_pages(body)
-        |> download_pages
+    case Tesla.get("http://mangareader.cc/chapter/#{chapter_link}") do
+      {:ok, %Tesla.Env{body: body, status: 200}} ->
+        body
 
       _ ->
-        {:err, "not found"}
+        {:error, "It was not possible to reach the page"}
     end
   end
 
@@ -29,23 +28,22 @@ defmodule ElixirMangaDownloadr.ChapterPage do
     |> Enum.with_index(fn element, index -> {index, element} end)
   end
 
-  def download_pages(pages_list) do
-    Enum.map(pages_list, fn page -> download_page(page) end)
-  end
+  # def download_pages(pages_list) do
+  #  Enum.map(pages_list, fn page -> download_page(page) end)
+  # end
 
-  def download_page(page_url) do
-    %HTTPotion.Response{body: body} = HTTPotion.get(page_url)
-    {:ok, body}
-  end
+  # def download_page(page_url) do
+  #  %HTTPotion.Response{body: body} = HTTPotion.get(page_url)
+  #  {:ok, body}
+  # end
 
-  def save_images(images_list) do
-    images_list
-    |> Enum.map(fn image -> save_image(image, @default_path) end)
-  end
+  # def save_images(images_list) do
+  #  images_list
+  #  |> Enum.map(fn image -> save_image(image, @default_path) end)
+  # end
 
-  # TODO
-  # Dar o nome das imagens dinamicamente
-  def save_image(image, path) do
-    File.write(path, image)
-  end
+  #
+  # def save_image(image, path) do
+  #  File.write(path, image)
+  # end
 end
