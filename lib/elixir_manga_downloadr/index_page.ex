0085395f2516c36d707bfa4d537620c1f1a2add5
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-03-11 22:32:41 -0300

    Disable the use of HTTPotion - to be refactored

--- lib/elixir_manga_downloadr/index_page.ex
+++ lib/elixir_manga_downloadr/index_page.ex
@@ -1,17 +1,11 @@
 defmodule ElixirMangaDownloadr.IndexPage do
-  @spec chapters(any) :: {:err, <<_::72>>} | {:ok, any, list}
-  def chapters(manga_name) do
-    case HTTPotion.get("mangareader.cc/manga/#{manga_name}") do
-      %HTTPotion.Response{body: body, headers: _headers, status_code: 200} ->
-        {:ok, fetch_manga_title(body), fetch_chapters(body)}
-
-      _ ->
-        {:err, "not found"}
-    end
-  end
+  #@spec chapters(any) :: {:err, <<_::72>>} | {:ok, any, list}
+  #def chapters(manga_name) do
+  #end
 
   def fetch_manga_title(html) do
-    {:ok, document} = Floki.parse_document(html)  
+    {:ok, document} = Floki.parse_document(html)
+      
     [{_, [{_, _}], [title]}] = Floki.find(document, "h1") 
     title
   end
