6f27ce9b0dd9db58d02a0e4094e5163a7df083e5
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-03-21 21:31:49 -0300

    Refactor the functions inside the IndexPage module
    Add todo to create pattern matching
    Refactor functions to use the returned values in a better way

--- lib/elixir_manga_downloadr/index_page.ex
+++ lib/elixir_manga_downloadr/index_page.ex
@@ -1,18 +1,20 @@
 defmodule ElixirMangaDownloadr.IndexPage do
   def index_page(manga_name) do
     case Tesla.get("http://mangareader.cc/manga/#{manga_name}") do
-      {:ok, %Tesla.Env{body: body, status: 200}} -> 
-        body
-        |> fetch_manga_title()
-        |> fetch_chapters()
-      _ -> {:error, "Wasn't possible to reach the page"}
+      {:ok, %Tesla.Env{body: body, status: 200}} ->
+        {:ok, document} = Floki.parse_document(body)
+        document
+
+      _ ->
+        {:error, "Wasn't possible to reach the page"}
     end
   end
-
-  def fetch_manga_title(html) do
-    {:ok, document} = Floki.parse_document(html)
-
-    [{_, [{_, _}], [title]}] = Floki.find(document, "h1")
+  
+  ## TODO: add pattern matching to the fetch_manga_title function,
+  # matching on {:ok, document} return of the Floki.parse_document
+  # and adding the match for the error case
+  def fetch_manga_title(html) do 
+    [{_, [{_, _}], [title]}] = Floki.find(html, "h1")
     title
   end
 
