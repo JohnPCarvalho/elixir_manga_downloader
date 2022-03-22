266a38f6a7d3b4860924e2b2c109f47a7acdd679
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-03-21 21:32:40 -0300

    Change field type

--- lib/elixir_manga_downloadr/mangas.ex
+++ lib/elixir_manga_downloadr/mangas.ex
@@ -1,19 +1,28 @@
 defmodule ElixirMangaDownloadr.Mangas do
 
-  
+  alias ElixirMangaDownloadr.Manga
   alias ElixirMangaDownloadr.IndexPage
-  
+  alias ElixirMangaDownloadr.ChapterPage
 
 
 
-  def get_manga(manga_name) do
-    IndexPage.fetch_manga_title(manga_name)
+  def create_manga(manga_name) do
+    manga_name =
+      IndexPage.index_page(manga_name)
+      |> IndexPage.fetch_manga_title()
+
+    chapters_list = 
+      IndexPage.index_page(manga_name)
+      |> IndexPage.fetch_chapters
+    
+
+    %Manga{manga_name: manga_name, chapters_list: chapters_list}
   end
 
   #def download_all_chapters() do
   #end
 
-  #defp create_folder() do
+  #defp create_folders(manga_path) do
   #end
 
   #defp manage_files() do
