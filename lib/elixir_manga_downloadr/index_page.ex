2e955a0983761aaa48a1d69f74c82ccfdb2b9e0b
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-03-24 08:57:26 -0300

    Add cleaner function

--- lib/elixir_manga_downloadr/index_page.ex
+++ lib/elixir_manga_downloadr/index_page.ex
@@ -9,17 +9,29 @@ defmodule ElixirMangaDownloadr.IndexPage do
         {:error, "Wasn't possible to reach the page"}
     end
   end
+
+  def get_manga_info(html) do
+    manga_title = fetch_manga_title(html)
+    chapters_list = fetch_chapters(html)
+
+    %{manga_title: manga_title, chapters_list: chapters_list}
+  end
   
   ## TODO: add pattern matching to the fetch_manga_title function,
   # matching on {:ok, document} return of the Floki.parse_document
   # and adding the match for the error case
-  def fetch_manga_title(html) do 
+  defp fetch_manga_title(html) do 
     [{_, [{_, _}], [title]}] = Floki.find(html, "h1")
     title
   end
 
-  def fetch_chapters(html) do
+  defp fetch_chapters(html) do
     Floki.find(html, "ul>li>span>a")
     |> Floki.attribute("href")
+    |> clean_chapters_list()
+  end
+
+  defp clean_chapters_list(chapters_list) do
+    Enum.filter(chapters_list, fn chapter -> chapter != "#" end)
   end
 end
