6b8625168b6a2b159763633bb3711cedc076b323
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-03-29 21:47:30 -0300

    Add functions to file module
    This module will handle the creation and movimentation of the
    files inside the application

--- lib/elixir_manga_downloadr/files/file.ex
+++ lib/elixir_manga_downloadr/files/file.ex
@@ -1,15 +1,25 @@
-defmodule ElixirMangaDownloadr.File do
-
-  # TODO: verificar o Sistema Operacional do usuário para escolher uma pasta "padrão"
-  # para salvar os arquivos.
-  
-  @unix_path "~/Documents/"
-  @mac_path "nao_sei_porra_nenhuma/"
-  @windows_path "c:/Users/Public/"
-
-  def create_folder(path) do
-    File.mkdir(path)
+defmodule ElixirMangaDownloadr.Files do
+  def set_manga_path(manga_name) do
+    File.cd("#{System.user_home()}/")
+    create_folder(manga_name)
+    get_manga_path
   end
 
-  
+  def get_manga_path() do
+    {:ok, path} = File.cwd()
+    path
+  end
+
+  def check_remaining_chapters(manga_path, chapters_list) do
+    {:ok, files} = File.ls(manga_path)
+    length(chapters_list) - length(files)
+  end
+
+  def create_folder(folder_name) do
+    case File.mkdir(folder_name) do
+      :ok -> File.cd(folder_name)
+      :eexist -> File.cd(folder_name)
+      {:error, _posix} -> "Something went wrong" 
+    end
+  end
 end
