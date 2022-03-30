2371ff693df74394f6bbb0a6721486a27adb3469
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-03-29 21:48:39 -0300

    Refactor download_all_chapters

--- lib/elixir_manga_downloadr/mangas.ex
+++ lib/elixir_manga_downloadr/mangas.ex
@@ -1,12 +1,13 @@
 defmodule ElixirMangaDownloadr.Mangas do
   alias ElixirMangaDownloadr.Manga
-  alias ElixirMangaDownloadr.IndexPage
-  alias ElixirMangaDownloadr.ChapterPage
+  import ElixirMangaDownloadr.IndexPage
+  import ElixirMangaDownloadr.ChapterPage
+  import ElixirMangaDownloadr.Files
 
   def create_manga(manga_name) do
     manga =
-      IndexPage.index_page(manga_name)
-      |> IndexPage.get_manga_info()
+      index_page(manga_name)
+      |> get_manga_info()
 
     %Manga{
       manga_name: manga.manga_title,
@@ -15,25 +16,16 @@ defmodule ElixirMangaDownloadr.Mangas do
   end
 
   def download_all_chapters(%Manga{manga_name: manga_name, chapters_list: chapters_list}) do
-    {:ok, absolute_path} = File.cwd()
-    File.mkdir("#{absolute_path}/#{manga_name}")
-    File.cd("#{absolute_path}/#{manga_name}")
+    manga_path = set_manga_path(manga_name)
 
     chapters_list
     |> Enum.with_index()
     |> Enum.map(fn {chapter, index} ->
-      File.mkdir("#{absolute_path}/#{manga_name}/#{index}")
-      File.cd("#{absolute_path}/#{manga_name}/#{index}")
-
-      ChapterPage.get_chapter_pages(chapter)
-      |> ChapterPage.fetch_pages()
-      |> ChapterPage.download_and_save_pages()
+      File.cd(manga_path)
+      create_folder("#{index + 1}")
+      get_chapter_pages(chapter)
+      |> fetch_pages()
+      |> download_and_save_pages
     end)
-  end
-
-  defp create_folders() do
-  end
-
-  defp manage_files() do
   end
 end
