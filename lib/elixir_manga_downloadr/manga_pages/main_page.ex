5cc7a5945f0b0de810874d7ce1e53f17adf7d080
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-04-06 23:39:33 -0300

    Add error handling and doc parsing

--- lib/elixir_manga_downloadr/manga_pages/main_page.ex
+++ lib/elixir_manga_downloadr/manga_pages/main_page.ex
@@ -1,9 +1,18 @@
 defmodule MainPage do
+  import Logger
   use Tesla
 
-  plug Tesla.Middleware.BaseUrl, "http://mangareader.cc/" 
+  plug(Tesla.Middleware.BaseUrl, "http://mangareader.cc/")
+  plug(Tesla.Middleware.FollowRedirects, max_redirects: 3)
 
   def main_manga_page(manga_name) do
-    get("manga/#{manga_name}")
+    case get("manga/#{manga_name}") do
+      {:ok, %Tesla.Env{body: body, status: 200}} ->
+        {:ok, parsed_body} = Floki.parse_document(body)
+        parsed_body
+
+      {:ok, %Tesla.Env{body: _body, status: 404}} ->
+        Logger.error("Page not found")
+    end
   end
 end
