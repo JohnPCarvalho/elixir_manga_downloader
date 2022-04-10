cd5788f874ea7dd1f3607f75eb4792b88dc3e3d1
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-04-10 19:12:01 -0300

    Add condition do download all chapters

--- lib/elixir_manga_downloadr/mangas.ex
+++ lib/elixir_manga_downloadr/mangas.ex
@@ -22,20 +22,23 @@ defmodule ElixirMangaDownloadr.Mangas do
   def download_all_chapters(%Manga{manga_name: manga_name, chapters_list: chapters_list}) do
     manga_path = set_manga_path(manga_name)
 
-    chapters_list
-    |> Enum.with_index()
-    |> Enum.map(fn {chapter, index} ->
-      File.cd(manga_path)
-      create_folder("#{index + 1}")
+    if needs_resuming_download?(manga_path, chapters_list) == false do
+      chapters_list
+      |> Enum.with_index()
+      |> Enum.map(fn {chapter, index} ->
+        File.cd(manga_path)
+        create_folder("#{index + 1}")
 
-      chapter_page(chapter)
-      |> fetch_pages()
-      |> download_and_save_pages
-    end)
+        chapter_page(chapter)
+        |> fetch_pages()
+        |> download_and_save_pages
+      end)
+    else
+      # resumes download functions called here...
+    end
   end
 
-  defp needs_resuming_download?() do
-      if check_remaining_chapter(manga_path, chapters_list) > 0 do
+  defp needs_resuming_download?(manga_path, chapters_list) do 
       false
     else
       true
