0c4fea375f60a5737e25baed1b08b7c1b5d8c248
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-04-20 00:12:50 -0300

    Add fixes to the code logic
    Changed the way that the chapter names will be fetched.
    Since it's no use trying to get the chapter name inside the
    span element because of the bad website strucure, now it's
    getting the Floki.text() of the anchor tag selection.
    It is more clean as well

--- lib/elixir_manga_downloadr/index_page.ex
+++ lib/elixir_manga_downloadr/index_page.ex
@@ -1,5 +1,6 @@
 defmodule ElixirMangaDownloadr.IndexPage do
   alias ElixirMangaDownloadr.MangaChapter
+
   def get_manga_info(html) do
     manga_title = get_manga_title(html)
     chapters_list = get_chapters(html)
@@ -15,28 +16,21 @@ defmodule ElixirMangaDownloadr.IndexPage do
     title
   end
 
-  def get_chapters(html) do
-    fetch_chapter_info(html)
-    |> clean_chapters_list()
-    |> order_chapters()
-
-  end
-
   defp order_chapters(chapters_list) do
     Enum.reverse(chapters_list)
   end
 
-
-  def fetch_chapter_info(html) do
-    Floki.find(html, "span.leftoff")
+  def get_chapters(html) do
+    Floki.find(html, "span.leftoff>a")
+    |> order_chapters()
     |> Enum.map(fn list_item ->
       chapter_title = Floki.text(list_item)
-      chapter_link = Floki.find(list_item, "a") |> Floki.attribute("href")
+
+      chapter_link =
+        Floki.find(list_item, "a")
+        |> Floki.attribute("href")
+
       %MangaChapter{chapter_title: chapter_title, chapter_link: chapter_link}
     end)
-  end
-
-  def clean_chapters_list(chapters_list) do
-    Enum.filter(chapters_list, fn chapter -> chapter != "#" end)
   end
 end
