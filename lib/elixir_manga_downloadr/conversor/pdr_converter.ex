ce044157367e16c57226ec57aa3994bcb99a1ca0
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-04-22 23:42:10 -0300

    Add PdfConverter module for test purposes

---
+++ lib/elixir_manga_downloadr/conversor/pdr_converter.ex
@@ -0,0 +1,17 @@
+defmodule PdfConverter do
+  @page_size [600, 450]
+  @image_size [600, 450]
+
+  def create_test_pdf() do
+    Pdf.build([size: [600, 450], compress: true], fn pdf ->
+      pdf
+      |> Pdf.set_info(title: "Demo PDF")
+      |> Pdf.add_page([600, 450])
+      |> Pdf.add_image(
+        {0, 0},
+        "/home/johnny/Neon Genesis Evangelion/Neon Genesis Evangelion Chapter 1/3.jpg", [100, 150]
+      )
+      |> Pdf.write_to("test.pdf")
+    end)
+  end
+end
