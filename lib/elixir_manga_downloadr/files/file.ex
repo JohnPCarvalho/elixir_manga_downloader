ce8ccbf43c6de51e2eb9478c5f80ce33895caac8
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-04-25 23:32:11 -0300

    Refactoring the module functions

--- lib/elixir_manga_downloadr/files/file.ex
+++ lib/elixir_manga_downloadr/files/file.ex
@@ -1,29 +1,30 @@
 defmodule ElixirMangaDownloadr.Files do
   @home_dir System.user_home()
 
-  def set_manga_path(manga_name) do
+  def set_manga_path(manga_title) do
     File.cd("#{@home_dir}/")
     # TODO: adicionar condicional para checar se a pasta já existe
-    if File.exists?("#{manga_name}") do
-      File.cd(manga_name)
+    if File.exists?("#{manga_title}") do
+      get_manga_path(manga_title)
     else
-      File.mkdir(manga_name)
-      File.cd(manga_name)
+      create_folder(manga_title)
+      File.cd(manga_title)
     end
 
-    get_manga_path()
+    get_manga_path(manga_title)
   end
 
   defp enter_manga_path(manga_title) do
-    File.cd("#{@home_dir}/#{manga_title}")
+    if File.exists?("#{@home_dir}/#{manga_title}") do
+      File.cd("#{@home_dir}/#{manga_title}")
+      :ok
+    else
+      IO.puts("This directory does not exist")
+      :error
+    end
   end
 
-  def get_manga_path() do
-    {:ok, path} = File.cwd()
-    path
-  end
-
-  def get_manga_path(manga_title) do
+  defp get_manga_path(manga_title) do
     enter_manga_path(manga_title)
     {:ok, path} = File.cwd()
     path
@@ -48,6 +49,5 @@ defmodule ElixirMangaDownloadr.Files do
   def create_folder(folder_name) do
     # TODO: separar essa função em duas. create_folder() deve apenas criar a pasta e não entrar nela também
     File.mkdir(folder_name)
-    File.cd(folder_name)
   end
 end
