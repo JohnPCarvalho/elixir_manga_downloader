35115eb67c2386673b0f71553fe034b19d2d6e8d
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-04-25 21:45:58 -0300

    Add new functions to PdfConverter module

--- lib/elixir_manga_downloadr/conversor/pdf_converter.ex
+++ lib/elixir_manga_downloadr/conversor/pdf_converter.ex
@@ -1,27 +1,35 @@
-defmodule PdfConverter do
+defmodule ElixirMangaDownloadr.PdfConverter do
+  alias ElixirMangaDownloadr.MangaChapter
+
   @page_size [600, 450]
   @image_size [600, 450]
 
+  def convert_all, do: "nothing for now"
 
-  def convert_to_pdf(%MangaChapter{chapter_title: chapter_title, chapter_link: _chapter_link}) do
-    Pdf.build([size: [600, 450], compress: true, fn pdf -> 
+  def generate_chapter_pdf(%MangaChapter{chapter_title: chapter_title}, chapter_pages) do
+    pdf = create_pdf(chapter_title)
+
+    Enum.map(chapter_pages, fn chapter_page ->
+      pdf
+      |> add_page()
+      |> add_image(chapter_page)
+    end)
+
+    Pdf.write_to(pdf, chapter_title)
+  end
+
+  def create_pdf(chapter_title) do
+    Pdf.build([size: @page_size, compress: true], fn pdf ->
       pdf
       |> Pdf.set_info(title: chapter_title)
-      |> Pdf.add_imge({0, 0}, image_path)
     end)
   end
 
-  def create_test_pdf() do
-    Pdf.build([size: [600, 450], compress: true], fn pdf ->
-      pdf
-      |> Pdf.set_info(title: "Demo PDF")
-      |> Pdf.add_page([600, 450])
-      |> Pdf.add_image(
-        {0, 0},
-        "/home/johnny/Neon Genesis Evangelion/Neon Genesis Evangelion Chapter 1/1.jpg", [100, 150]
-      )
-      |> Pdf.write_to("test.pdf")
-    end)
+  def add_page(pdf) do
+    Pdf.add_page(pdf, @page_size)
   end
 
+  def add_image(_pdf, image_path) do
+    Pdf.add_image({0, 0}, image_path)
+  end
 end
