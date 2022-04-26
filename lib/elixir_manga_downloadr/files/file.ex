0acd8f59d1c211cd5b3a81e0f76f324a6261fa97
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-04-25 22:38:18 -0300

    Update and add more functions to the Files module
    In order to support the new PdfConverter module, new functions
    to handle File related operations will be needed

--- lib/elixir_manga_downloadr/files/file.ex
+++ lib/elixir_manga_downloadr/files/file.ex
@@ -1,6 +1,8 @@
 defmodule ElixirMangaDownloadr.Files do
+  @home_dir System.user_home()
+
   def set_manga_path(manga_name) do
-    File.cd("#{System.user_home()}/")    
+    File.cd("#{@home_dir}/")
     # TODO: adicionar condicional para checar se a pasta já existe
     if File.exists?("#{manga_name}") do
       File.cd(manga_name)
@@ -8,12 +10,29 @@ defmodule ElixirMangaDownloadr.Files do
       File.mkdir(manga_name)
       File.cd(manga_name)
     end
+
     get_manga_path()
+  end
+
+  defp enter_manga_path(manga_title) do
+    File.cd("#{@home_dir}/#{manga_title}")
   end
 
   def get_manga_path() do
     {:ok, path} = File.cwd()
     path
+  end
+
+  def get_manga_path(manga_title) do
+    enter_manga_path(manga_title)
+    {:ok, path} = File.cwd()
+    path
+  end
+
+  def get_all_chapter_paths(manga_title) do
+    enter_manga_path(manga_title)
+    {:ok, chapters_list} = File.ls()
+    chapters_list
   end
 
   def remaining_chapters(manga_path, chapters_list) do
@@ -26,9 +45,9 @@ defmodule ElixirMangaDownloadr.Files do
     length(files)
   end
 
-
   def create_folder(folder_name) do
+    # TODO: separar essa função em duas. create_folder() deve apenas criar a pasta e não entrar nela também
     File.mkdir(folder_name)
     File.cd(folder_name)
-    end
+  end
 end
