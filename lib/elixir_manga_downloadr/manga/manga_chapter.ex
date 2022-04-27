7ad157f5be19b6bbb2621082c1bcbdea8efd1404
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-04-26 21:43:56 -0300

    Add chapter_dir property

--- lib/elixir_manga_downloadr/manga/manga_chapter.ex
+++ lib/elixir_manga_downloadr/manga/manga_chapter.ex
@@ -1,6 +1,5 @@
 defmodule ElixirMangaDownloadr.MangaChapter do
-  defstruct [
-    chapter_title: nil,
-    chapter_link: nil
-  ] 
+  defstruct chapter_title: nil,
+            chapter_link: nil,
+            chapter_dir: nil
 end
