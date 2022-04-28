49d0f59237ab50944d5d28ee304314528e22672e
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-04-28 07:55:01 -0300

    Remake whole module from scratch
    I determined that I needed to search in the akitaonrails repo
    to learn how to do the pdf generation

--- lib/elixir_manga_downloadr/conversor/pdf_converter.ex
+++ lib/elixir_manga_downloadr/conversor/pdf_converter.ex
@@ -1,36 +1,60 @@
 defmodule ElixirMangaDownloadr.PdfConverter do
   alias ElixirMangaDownloadr.Files
-  alias ElixirMangaDownloadr.MangaChapter
 
-  @page_size [600, 450]
-  @image_size [600, 450]
+  # Kindle maximum resolution
+  @image_dimensions "600x800"
+  # comfortable PDF file number of pages
+  @pages_per_volume 250
+  # the best value is probably the total number of CPU cores
+  @maximum_pdf_generation 2
+  # has to wait for huge number of async Tasks at once
+  @await_timeout_ms 1_000_000
 
-  def convert_all, do: "nothing for now"
+  def optimize_images(directory) do
+    Logger.debug("Running mogrify to convert all images down to Kindle supported size (600x800)")
+    Porcelain.shell("mogrify -resize #{@image_dimensions} #directory/*.jpg")
+    directory
+  end
 
-  def generate_chapter_pdf(%MangaChapter{chapter_title: chapter_title}, chapter_pages) do
-    pdf = create_pdf(chapter_title)
+  def compile_pdfs(directory, manga_name) do
+    {:ok, final_files_list} = File.ls(directory)
 
-    Enum.map(chapter_pages, fn chapter_page ->
-      pdf
-      |> add_page()
-      |> add_image(chapter_page)
+    final_files_list
+    |> Enum.sort()
+    |> Enum.map(&"#{directory}/#{&1}")
+    |> chunk(@pages_per_volume)
+    |> Enum.with_index()
+    |> chunk(@maximum_pdf_generation)
+    |> Enum.map(fn batch ->
+      batch
+      |> Enum.map(&compile_volume(manga_name, directory, &1))
+      |> Enum.map(&Task.await(&1, @await_timeout_ms))
     end)
 
-    Pdf.write_to(pdf, chapter_title)
+    directory
   end
 
-  def create_pdf(chapter_title) do
-    Pdf.build([size: @page_size, compress: true], fn pdf ->
-      pdf
-      |> Pdf.set_info(title: chapter_title)
+  defp compile_volume(manga_name, directory, {chunk, index}) do
+    {:ok, convert_cmd} = prepare_volume(manga_name, directory, chunk, index)
+    Logger.debug("Compiling volume #{index + 1}.")
+    Task.async(fn -> Porcelain.shell(convert_cmd) end)
+  end
+
+  defp prepare_volume(manga_name, directory, chunk, index) do
+    volume_directory = "#{directory}/#{manga_name}_#{index + 1}"
+    volume_file = "#{volume_directory}.pdf"
+    File.mkdir_p(volume_directory)
+
+    Enum.each(chunk, fn file ->
+      [destination_file | _rest] = String.split(file, "/") |> Enum.reverse()
+      File.rename(file, "#{volume_directory}/#{destination_file}")
     end)
+
+    {:ok, "convert #{volume_directory}/*.jpg #{volume_file}"}
   end
 
-  def add_page(pdf) do
-    Pdf.add_page(pdf, @page_size)
-  end
-
-  def add_image(pdf, image_path) do
-    Pdf.add_image(pdf, {0, 0}, image_path)
+  defp chunk(collection, default_size) do
+    size = [Enum.count(collection), default_size] |> Enum.min()
+    Enum.chunk(collection, size, size, [])
   end
 end
