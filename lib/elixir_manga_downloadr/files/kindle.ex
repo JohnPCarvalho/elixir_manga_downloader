7ba6acd827be6d86a687644e25e81d2a5356aeca
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-05-22 23:43:32 -0300

    Add linux distros Kindle path

--- lib/elixir_manga_downloadr/files/kindle.ex
+++ lib/elixir_manga_downloadr/files/kindle.ex
@@ -3,5 +3,8 @@ defmodule ElixirMangaDownloadr.Kindle do
   end
 
   defp get_kindle_path() do
+    [username, _rest] = System.user_home() |> String.split("/") |> Enum.reverse()
+    # Kindle path in Linux distros
+    File.cd("/media/#{username}/Kindle")
   end
 end
