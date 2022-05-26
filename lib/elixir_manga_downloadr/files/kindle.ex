4e7573e89fb5748b81640bdadb346e5cbd403f08
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-05-25 22:04:27 -0300

    WIP of kindle module

--- lib/elixir_manga_downloadr/files/kindle.ex
+++ lib/elixir_manga_downloadr/files/kindle.ex
@@ -1,10 +1,27 @@
 defmodule ElixirMangaDownloadr.Kindle do
-  def save() do
+  alias ElixirMangaDownloadr.Files
+
+  def save(manga_name) do
+    case get_kindle_path() do
+      {:error, :enoent} -> "It isn't okay"
+      :ok -> "It's alright now"
+    end
+  end
+
+  def greater_function_that_i_will_name_later() do
+    Files.enter_manga_path(manga_name)
+    {:ok, list_of_files} = File.ls()
+
+    Enum.each(list_of_files, fn pdf_chapter ->
+      IO.inspect(pdf_chapter)
+    end)
+
+    IO.inspect(get_kindle_path())
   end
 
   defp get_kindle_path() do
-    [username, _rest] = System.user_home() |> String.split("/") |> Enum.reverse()
     # Kindle path in Linux distros
-    File.cd("/media/#{username}/Kindle")
+    File.cd("/media/#{Files.get_username()}/Kindle")
+    # TODO: get other OS's Kindle path
   end
 end
