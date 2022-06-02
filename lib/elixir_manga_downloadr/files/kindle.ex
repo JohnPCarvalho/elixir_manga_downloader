816f6784f06265ba94edddfa559f8abdcebcfb0d
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-06-02 09:12:38 -0300

    Refactor code

--- lib/elixir_manga_downloadr/files/kindle.ex
+++ lib/elixir_manga_downloadr/files/kindle.ex
@@ -1,28 +1,29 @@
 defmodule ElixirMangaDownloadr.Kindle do
   alias ElixirMangaDownloadr.Files
 
-  def save(manga_name, kindle) when kindle == true do
-    case enter_kindle_path() do
-      :ok ->
-        move_manga_to_kindle(manga_name)
+  @kindle_path "/media/#{Files.get_username()}/Kindle/documents"
 
-      {:error, :enoent} ->
-        "Kindle path was not found"
-        # faz o restante do processo
+  def save(manga_path, kindle) when kindle == true do
+    if File.exists?(@kindle_path) do
+      move_manga_to_kindle(manga_path)
+    else
+      move_manga_to_documents(manga_path)
     end
   end
 
-  def move_manga_to_kindle(manga_path) do
-    {:ok, kindle_path} = get_kindle_path()
-    manga_name = Path.basename(manga_path)
-    File.cp_r(manga_path, "#{kindle_path}/#{manga_name}/")
+  defp move_manga_to_documents(manga_path) do
+    File.cp_r(manga_path, "/home/johnny/Documents/Clannad")
   end
 
-  defp get_kindle_path() do
-    File.cwd()
+  defp move_manga_to_kindle(manga_path) do
+    manga_name = Path.basename(manga_path)
+    File.cp_r(manga_path, "#{@kindle_path}/#{manga_name}/")
   end
 
   defp enter_kindle_path() do
-    File.cd("/media/#{Files.get_username()}/Kindle/documents")
+    case File.cd("/media/#{Files.get_username()}/Kindle/documents") do
+      :ok -> {File.cwd(), :found}
+      {:error, :enoent} -> :not_found
+    end
   end
 end
