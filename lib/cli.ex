cf1377d24bf8861a4835dafb8cea1059719c7052
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-06-04 18:40:06 -0300

    Change cli.ex dir

---
+++ lib/cli.ex
@@ -0,0 +1,8 @@
+defmodule ElixirMangaDownloadr.CLI do
+  def main(args) do
+    options = [switches: [file: :string], aliases: [f: :file]]
+
+    {opts, _, _} = OptionParser.parse(args, options)
+    IO.inspect opts, label: "Command Line Arguments"
+  end
+end
