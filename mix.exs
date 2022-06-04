e3a7221329e3462a1aa66eb25085552689192190
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-06-03 23:58:54 -0300

    Add initial CLI parser code

--- mix.exs
+++ mix.exs
@@ -7,6 +7,9 @@ defmodule ElixirMangaDownloadr.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+
+      escript: [main_module: Commandline.CLI],
+
       deps: deps()
     ]
   end
