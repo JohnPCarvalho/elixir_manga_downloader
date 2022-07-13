d82b4f5f3f6de21fdde50f8408ed1c6dca18d4d7
Johnny Carvalho <johnnypcarvalho@gmail.com>
2022-07-12 22:52:16 -0300

    Add future plannings

--- lib/workflow.ex
+++ lib/workflow.ex
@@ -8,6 +8,9 @@ defmodule ElixirMangaDownloadr.Workflow do
     manga_slug example: "neon-genesis-evangelion", "dragon-ball", "hunter-x-hunter"
   """
   def process_manga(manga_slug) do
+    #TODO: add possibility to send books to e-mail
+    # Adicionar feedback de instruções para configurar o envio por e-mail
+
     Mangas.create_manga(manga_slug)
     |> Mangas.download_all_chapters()
     |> PdfConverter.convert_chapters()
