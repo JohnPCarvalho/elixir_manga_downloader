aa4263d0b7b342c1be69c16473b7170af9f4fbda
Johnny Carvalho <johnnypcarvalho@gmail.com>
2022-07-17 21:56:53 -0300

    Add new test files

---
+++ test/manga_wrapper/manga_pages/index_page_test.exs
@@ -0,0 +1,16 @@
+defmodule ElixirMangaDownloadr.IndexPageTest do
+
+  alias ElixirMangaDownloadr.IndexPage
+  alias ElixirMangaDownloadr.MangaChapter
+
+  describe "get_manga_info/1" do
+    test "returns a tuple with manga_title and list of chapters" do
+    end
+  end
+
+
+  describe "get_chapters/1" do
+    test "get the chapters list and returns it in the MangaChapter struct" do
+    end
+  end
+end
