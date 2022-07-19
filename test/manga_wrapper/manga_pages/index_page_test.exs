09e7a756d92390d785fc38d714382db337501bd7
Johnny Carvalho <johnnypcarvalho@gmail.com>
2022-07-18 23:34:27 -0300

    Add basic module vars to test

--- test/manga_wrapper/manga_pages/index_page_test.exs
+++ test/manga_wrapper/manga_pages/index_page_test.exs
@@ -3,6 +3,13 @@ defmodule ElixirMangaDownloadr.IndexPageTest do
   alias ElixirMangaDownloadr.IndexPage
   alias ElixirMangaDownloadr.MangaChapter
 
+  @sample_manga_info %{manga_title: "Clannad", chapters_list: [@sample_manga_chapter, @sample_manga_chapter]}
+
+  @sample_manga_chapter %MangaChapter{
+    chapter_title: "Clannad Chapter 1",
+    chapter_link: "www.clannad.com/chapter1"
+  }
+
   describe "get_manga_info/1" do
     test "returns a tuple with manga_title and list of chapters" do
     end
