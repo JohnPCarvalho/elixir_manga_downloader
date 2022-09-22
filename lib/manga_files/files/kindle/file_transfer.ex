624a4316b96d96ed3c6b63afb8790410fb6bf152
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-09-22 00:14:38 -0300

    Add docs

--- lib/manga_files/files/kindle/file_transfer.ex
+++ lib/manga_files/files/kindle/file_transfer.ex
@@ -3,4 +3,7 @@ defmodule ElixirMangaDownloadr.MangaFiles.Kindle.FileTransfer do
   Module created to handle the Kindle use cases in the application.
   This module is responsible for the physical and e-mail file transfers
   """
+
+  #TODO: Separar as funções pensando nas duas funcionalidades: enviar direto para o kindle por USB ou enviar por e-mail.
+  # No caso do e-mail será necessário um módulo novo para lidar com estas conexões.
 end
