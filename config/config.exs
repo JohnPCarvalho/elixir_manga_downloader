83df71294564986c90599c8b830efc42d3a0af3e
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-09-26 21:53:53 -0300

    Add config files

--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,11 @@
 import Config
+
+config :elixir_manga_downloadr, ElixirMangaDownloadr.Mailer,
+  adapter: Bamboo.SMTPAdapter,
+  server: "smtp.domain",
+  port: 1025,
+  username: System.get_env("SMTP_USERNAME"),
+  password: System.get_env("SMTP_PASSWORD"),
+  tls: :if_available, # can be `:always` or `:never`
+  ssl: false, # can be `true`
+  retries: 1
